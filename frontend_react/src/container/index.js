4c3e2fbc41dd605e5ba4fae91a04f8bf3c5855b6
Mohammad Hossein <mohammad.hossein0999@gmail.com>
2024-04-01 01:29:44 -0400

    Remove Testimonial section

--- frontend_react/src/container/index.js
+++ frontend_react/src/container/index.js
@@ -3,7 +3,6 @@ import About from './About/About';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Skills from './Skills/Skills';
-import Testimonial from './Testimonial/Testimonial';
 import Work from './Work/Work';
 
 export {
@@ -11,7 +10,6 @@ export {
   Footer,
   Header,
   Skills,
-  Testimonial,
   Work,
 };
 
